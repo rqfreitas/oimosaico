4994e384b9d76a50708274f3fd78efaaf9ff0ee3
Rodrigo Freitas <rodrigofreitas@Rodrigos-Laptop.local>
2025-04-10 13:10:02 -0300

    vite inicio2

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+
 import './App.css'
 import { useEffect } from "react";
 
@@ -8,7 +7,7 @@ function App() {
   useEffect(() => {
     window.location.href = "https://linktr.ee/mosaicoigreja";
   }, []);
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
